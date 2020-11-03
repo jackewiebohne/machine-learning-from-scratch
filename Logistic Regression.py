5f7acdcd0cf41cffa474db35195cdf69677818cb
jackewiebohne <69870039+jackewiebohne@users.noreply.github.com>
2020-11-03 10:19:49 +0100

    Update Logistic Regression.py

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -29,14 +29,32 @@ class LogisticRegression():
 
         if GD_batch_size != self.m:
 
+            shuffler = np.random.permutation(self.X.shape[0]) # to shuffle both X and y the same way
+            X_shuffled = self.X[shuffler]
+            y_shuffled = self.y[shuffler]
+            no_full_batches = self.m//GD_batch_size
+            X_batches = []
+            Y_batches = []
+
+            for i in range(no_full_batches):
+                X_batches.append(X_shuffled[i * GD_batch_size : GD_batch_size * (i+1), :])
+                Y_batches.append(y_shuffled[i * GD_batch_size : GD_batch_size * (i+1)])
+
+            no_remaining = self.m % GD_batch_size #in case the batch size does not divide the # of training samples evenly
+            X_batches.append(X_shuffled[-no_remaining :,:])
+            Y_batches.append(y_shuffled[-no_remaining :])
+
+
+
             for iteration in range(self.max_iter):
 
-                theta_shift = np.concatenate(([0], self.theta[1:])) # since the intercept is not regularised we multiply the regularisation parameter lmbda with theta_shift with a 0 inserted at the position of theta_0/the intercept
-                index = random.randint(0, (self.m - (GD_batch_size) ) ) # generate a random number that will be used as index for slicing the training examples m # the subtraction is to avoid running out of range, this random number+GD_batch_size will be the slicing scope of the training samples
-                h_batch = self.Sigmoid(self.X[index : index + GD_batch_size] @ self.theta)
-                reg = (self.lmbda/GD_batch_size) * theta_shift # regularisation
-                grad = ( (1/GD_batch_size) * ( ( (h_batch - self.y[index : index + GD_batch_size]) @ self.X[index : index + GD_batch_size]) ) ) + reg
-                self.theta = self.theta - (alpha * grad)
+                for j in range(len(X_batches)):
+
+                    theta_shift = np.concatenate(([0], self.theta[1:])) # since the intercept is not regularised we multiply the regularisation parameter lmbda with theta_shift with a 0 inserted at the position of theta_0/the intercept
+                    h_batch = self.Sigmoid(X_batches[j] @ self.theta)
+                    reg = (self.lmbda/X_batches[j].shape[0]) * theta_shift # regularisation
+                    grad = ( (1/X_batches[j].shape[0]) * ( ( (h_batch - Y_batches[j]) @ X_batches[j]) ) ) + reg
+                    self.theta = self.theta - (alpha * grad)
 
                 #appending the terms
                 thetas.append(self.theta)
@@ -53,6 +71,7 @@ class LogisticRegression():
                 reg = (self.lmbda/self.m) * theta_shift
                 grad = ( (1/self.m) * ( (h - self.y) @ self.X) ) + reg
                 self.theta = self.theta - (alpha * grad)
+                # print(grad)
 
                 #appending the terms
                 thetas.append(self.theta)
@@ -66,16 +85,19 @@ class LogisticRegression():
     def CostFunction(self, learning_rate=0.01, GD_batch_size=100):
         thetas, theta_shifts, grads, iterations = self.GradientDescent(learning_rate, GD_batch_size)
         costs = []
+        #costs2 = []
 
         for iteration in range(self.max_iter):
 
             h = self.Sigmoid(self.X @ thetas[iteration])
             reg = (self.lmbda/(2 * self.m) ) * (theta_shifts[iteration] @ theta_shifts[iteration])
             J = (1/self.m) * ( (-self.y).T @ np.log(h) - (1 - self.y).T @ np.log(1 - h) ) + reg
+            #alternative solution: #J2 = -(1/self.m) * np.sum(self.y * np.log(h) + (1 - self.y) * np.log(1 - h), keepdims=True)
 
             costs.append(J)
+            # costs2.append(J2)
 
-        return costs, grads, iterations
+        return costs, grads, iterations #, costs2
 
 
     def Predict(self):
